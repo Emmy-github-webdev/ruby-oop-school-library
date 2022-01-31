1b762553f314484ad57b017d1d30921ec663772d
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-01-31 17:43:10 +0100

    Add setter and getter

--- person.rb
+++ person.rb
@@ -1,10 +1,43 @@
 class Person
-
+  
   def initialize(name = "Unkown", age, parent_permission = true)
-    
+    @id = Random.rand(1..1000)
+    @name = name
+    @parent_permission = parent_permission
+    @age = age
+  end
+
+  @id
+  @name
+  @age
+
+  #Setter
+  def name=(name)
+    @name = name
+  end
+
+  def age=(age)
+    @name = name
+  end
+
+  #getter
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
   end
 
   def to_s
     "Hello! My name is #{@name}, and I'm #{age} years old."
   end
 end
+
+person1 = Person.new
+person1.name = "Emmanuel"
+puts person1.name
