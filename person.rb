e6846247bbaaf5c056ccda75127fb4f163de8f32
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-01-31 20:30:37 +0100

    Add student class inheriting person class

--- person.rb
+++ person.rb
@@ -1,45 +1,21 @@
 class Person
-  attr_writer :id
   attr_accessor :name, :age
-  
-  def initialize(age, name: 'Unkown',  parent_permission: true)
+  attr_reader :id
+
+  def initialize(age, name = 'Unkown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
+    @parent_permission = parent_permission
     @age = age
   end
 
-  #Setter
-  def name=(name)
-    @name = name
-  end
-
-  def age=(age)
-    @name = name
-  end
-
-  #getter
-  def id
-    @id
-  end
-
-  def name
-    @name
-  end
-
-  def age
-   print  @age
-  end
-
   def can_use_services?
-    age >= 18 || parent_permission == true ? true : false
+    age >= 18 || parent_permission == true
   end
 
   private
 
-  def is_of_age?
-    age >= 18 ? true : false
+  def of_age?
+    age >= 18
   end
-
 end
-
-person1 = Person.new(20)
