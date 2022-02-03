c687817263553a25bdb0ec4b1f85de06b4156533
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-03 10:53:05 +0100

    Add a choose option method to app class

--- app.rb
+++ app.rb
@@ -10,21 +10,13 @@ class App
   def choose_a_number
     puts
     puts 'Please choose an option by entering a number:'
-    puts
     puts '1 - List all books'
-    puts
     puts '2 - List all people'
-    puts
     puts '3 - Create a person'
-    puts
     puts '4 - Create a book'
-    puts
     puts '5 - Create a rental'
-    puts
     puts '6 - List all rentals for a given person id'
-    puts
     puts '7 - Exit'
-
   end
 end
 
