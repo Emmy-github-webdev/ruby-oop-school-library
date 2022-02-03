65ffc0b0a41c42186241b7202b3f7c4c6d63c0df
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-03 11:01:53 +0100

    Add action method to be perform based on the selection number  to app class

--- app.rb
+++ app.rb
@@ -18,5 +18,24 @@ class App
     puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
   end
+
+  def action_based_on_number_choosed
+    case option
+    when '1'
+      list_all_books
+    when '2'
+      list_all_people
+    when '3'
+      create_a_person
+    when '4'
+      create_a_book
+    when '5'
+      create_a_rental
+    when '6'
+      list_all_rentals_for_a_person
+    else
+      exit
+    end
+  end
 end
 
