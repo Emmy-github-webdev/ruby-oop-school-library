45fd2bdf8e3cfe5dc55111f69e94c6ba594ab03f
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-08 19:47:41 +0100

    Add new teacher function

--- input_output.rb
+++ input_output.rb
@@ -24,11 +24,27 @@ class IO
   end
 
   def add_new_student(person)
-    @people_array << {'id' => person.id, 'age' => person.age, 'name' => person.name, 'classroom' => person.classroom, 'parent_permission' => person.parent_permission}
+    @people_array << {
+      'id' => person.id, 
+      'age' => person.age, 
+      'name' => person.name, 
+      'classroom' => person.classroom, 
+      'parent_permission' => person.parent_permission
+    }
+  end
+
+  def add_new_teacher(person)
+    @people_array << {
+      'id' => person.id,
+      'specialization' => person.specialization,
+      'name' => person.name,
+      'age' => person.age
+    }
   end
 
   def add_new_book
     @add_book.books.each { |book| @book_array << { 'title' => book.title, 'author' => book.author } }
     File.write('data/book.json', JSON.dump(@book_array))
   end
+
 end
