c0ffd30582e78e19d96dca5f654122e0a954ff16
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-09 10:35:49 +0100

    Update the main class

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
-#!/usr/bin/env ruby
-
-require './app'
+require_relative 'input-output/input_output_operation'
 
 def main
-  app = App.new
-  app.run_app
+  optionselected = DisplayMenuOptions.new
+  loop do
+    optionselected.menu
+    user_choice = gets.chomp.to_i
+
+    optionselected.option(user_choice)
+    break if user_choice == 7
+  end
 end
 
 main
