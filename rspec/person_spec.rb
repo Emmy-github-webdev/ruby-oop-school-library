331d4f801ef3bf43d41dd49470f04ad5fa1f51e3
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-10 08:24:05 +0100

    Update test for person class

--- rspec/person_spec.rb
+++ rspec/person_spec.rb
@@ -8,5 +8,11 @@ describe Person do
       person_info = [!person.id.nil?, person.age, person.name, person.parent_permission]
       expect(person_info).to eq [true, 18, 'Unkown', true]
     end
+    it 'A person should be initailized by name, age, id' do
+      id = rand(1..1000)
+      person = Person.new(21, 'Emmanuel', id)
+      person_info = [person.id, person.age, person.name, person.parent_permission]
+      expect(person_info).to eq [id, 21, 'Emmanuel', true]
+    end
   end
 end
